33be987c641ec3f1366d078aa9fb9857e8d01ec0
Carlos Paredes <cparedesr@uc.cl>
2023-09-23 18:34:23 -0300

    feat(): migrate to fastapi

---
+++ app/web/api/router.py
@@ -0,0 +1,8 @@
+from fastapi.routing import APIRouter
+
+from app.web.api import docs, monitoring, users
+
+api_router = APIRouter()
+api_router.include_router(monitoring.router)
+api_router.include_router(users.router)
+api_router.include_router(docs.router)
