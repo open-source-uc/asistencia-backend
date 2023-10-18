9a2e4b59369fdd679463f3f36a30357705012780
Benjamín Vicente <benjavicente@uc.cl>
2023-10-18 19:47:47 -0300

    feat(sheets): initial spreadsheet endpoint

--- app/db/dao/course_student_activity_record.py
+++ app/db/dao/course_student_activity_record.py
@@ -1,11 +1,11 @@
 from typing import List, Optional
 
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.dependencies import get_db_session
-from app.db.models import CourseStudentActivityRecord, CourseActivity, Student
+from app.db.models import CourseActivity, CourseStudentActivityRecord, Student
 from app.schemas.course_student_activity_record import CourseStudentActivityRecordCreate
 
 
@@ -88,3 +88,43 @@ class CourseStudentActivityRecordDAO:
             query = query.where(CourseStudentActivityRecord.student_id == student_id)
         rows = await self.session.execute(query)
         return list(rows.scalars().fetchall())
+
+    async def get_activity_matrix(
+        self, course_id: str, students_attendance_ids: List[str], activities: List[str]
+    ):
+        """
+        Get the [student] x [activity] matrix.
+        The returned array should be a list of lists, where each list
+        represents a student and each element in the list represents an activity.
+        """
+        # TODO: match with hashed ids
+        # TODO: add something to allow matching the order of the students (return back the attendances ids?)
+        # TODO: fix bug where it fills the matrix with the same value
+        query = (
+            select(
+                Student.id,
+                func.json_object_agg(
+                    CourseActivity.slug,
+                    func.coalesce(CourseStudentActivityRecord.id, None),
+                ).label("activities"),
+            )
+            .join(
+                CourseStudentActivityRecord,
+                Student.id == CourseStudentActivityRecord.student_id,
+            )
+            .join(
+                CourseActivity,
+                CourseActivity.course_id == Student.course_id,
+            )
+            .where(
+                Student.course_id == course_id,
+                CourseActivity.slug.in_(activities),
+                Student.attendance_id.in_(students_attendance_ids),
+            )
+            .group_by(
+                Student.id,
+            )
+        )
+
+        rows = await self.session.execute(query)
+        return {k: v for k, v in rows.fetchall()}
---
+++ app/web/api/spreadhsets/views.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+
+from app.db.dao.course_student_activity_record import CourseStudentActivityRecordDAO
+
+router = APIRouter()
+
+
+class SpreadsheetsPayload(BaseModel):
+    course_id: str
+    students_ids: list[str]
+    activities_slugs: list[str]
+
+
+@router.post("/check_assistance", tags=["spreadhsets"])
+async def check_assistance(
+    payload: SpreadsheetsPayload, dao: CourseStudentActivityRecordDAO = Depends()
+):
+    """
+    Checks the assistance given a user_id rows
+    and activity_slug columns.
+    """
+
+    return await dao.get_activity_matrix(
+        activities=payload.activities_slugs,
+        students_attendance_ids=payload.students_ids,
+        course_id=payload.course_id,
+    )
