83f98a12caf0504562cc7ca6a4d6e13118848bca
Carlos Paredes <carlos@paredesrobles.com>
2023-11-06 19:40:22 -0300

    feat(User): add authorization middleware

--- app/db/dao/user_course.py
+++ app/db/dao/user_course.py
@@ -5,7 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db.dependencies import get_db_session
-from app.db.models import UserCourse
+from app.db.models import UserCourse, Course
 from app.schemas.user_course import UserCourseCreate
 
 
@@ -55,3 +55,41 @@ class UserCourseDao:
         )
 
         return list(raw_records.scalars().fetchall())
+
+    async def get_all_courses_for_user(
+        self, user, limit: int = 100, offset: int = 0
+    ) -> List[Course]:
+        """
+        Get all UserCourse with limit/offset pagination.
+        """
+
+        raw_records = await self.session.execute(
+            select(Course)
+            .join(UserCourse)
+            .where(UserCourse.user_email == user.email)
+            .limit(limit)
+            .offset(offset),
+        )
+
+        return list(raw_records.scalars().fetchall())
+
+    async def authorize_mail_for_course(
+        self, course_id: str, user_email: str, allowed_roles: Optional[List[str]] = None
+    ) -> bool:
+        """
+        Check if user is authorized to access course.
+        """
+
+        user_course = (await self.session.execute(
+            select(UserCourse)
+            .where(UserCourse.course_id == course_id)
+            .where(UserCourse.user_email == user_email)
+        )).scalar_one_or_none()
+
+        if not user_course:
+            return False
+
+        if allowed_roles and user_course.role not in allowed_roles:
+            return False
+
+        return True
