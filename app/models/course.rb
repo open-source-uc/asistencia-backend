ffabb6ae1b8ddab7c79dd0d8679a50ef454ba2f9
Carlos Paredes <carlos@paredesrobles.com>
2023-11-23 00:13:27 -0300

    feat(Course): add course

---
+++ app/models/course.rb
@@ -0,0 +1,14 @@
+class Course < ApplicationRecord
+  validates :name, presence: true, uniqueness: true 
+end
+
+# == Schema Information
+#
+# Table name: courses
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  enabled    :string           default("t"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_23_024810) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_23_030836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_23_024810) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "enabled", default: "t", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
