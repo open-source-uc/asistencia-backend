767678cd8f4762d50f863a04c572f4ff3e08d321
Carlos Paredes <carlos@paredesrobles.com>
2023-11-28 21:55:49 -0300

    fix(): allow active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include PowerTypes::Presentable
   protect_from_forgery unless: -> { request.format.json? }
   before_action :configure_permitted_parameters, if: :devise_controller?
-  respond_to :json
+  respond_to :json, :html
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
