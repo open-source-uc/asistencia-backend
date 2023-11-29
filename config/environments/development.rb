36de1807e3536199b352d988b50c3ddb2fdd8dd8
Carlos Paredes <carlos@paredesrobles.com>
2023-11-28 21:36:42 -0300

    chore(): add domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,5 @@ Rails.application.configure do
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
   config.assets.quiet = true
+  config.hosts << "attendance.cparedesr.com"
 end
