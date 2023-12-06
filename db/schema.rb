cb7afb466bfa63b20f3b144703dae11aa9ae48be
Carlos Paredes <carlos@paredesrobles.com>
2023-12-06 18:17:06 -0300

    feat(Activity): uniqueness by course and slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_020155) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_06_211451) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_020155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.index ["course_id", "slug"], name: "index_activities_on_course_id_and_slug", unique: true
     t.index ["course_id"], name: "index_activities_on_course_id"
     t.index ["slug"], name: "index_activities_on_slug", unique: true
   end
